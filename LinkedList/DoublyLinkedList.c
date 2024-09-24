285aaf89f13009340e6e2d222b1ab499608ee1a4
Simon Lee <simon_lee@gss.com.tw>
2024-09-24 18:25:32 +0800

    add delete implementation of doubly linked list

--- LinkedList/DoublyLinkedList.c
+++ LinkedList/DoublyLinkedList.c
@@ -56,6 +56,37 @@ linkedlist *prepend(linkedlist *list, int data) {
     return list;
 }
 
+linkedlist *deleteforward(linkedlist *list, int target) {
+    node *curr = list->head;
+    while (curr->next != list->head) {
+        // target found
+        if (curr->next->data == target) {
+            node *tmp = curr->next;
+            curr->next->next->prev = curr;
+            curr->next = curr->next->next;
+            free(tmp);
+            break;
+        }
+        curr = curr->next;
+    }
+    return list;
+}
+
+linkedlist *deletebackward(linkedlist *list, int target) {
+    node *curr = list->head;
+    while (curr->prev != list->head) {
+        // target found
+        if (curr->prev->data == target) {
+            node *tmp = curr->prev;
+            curr->prev->prev->next = curr;
+            curr->prev = curr->prev->prev;
+            free(tmp);
+            break;
+        }
+        curr = curr->prev;
+    }
+    return list;
+}
 void printforward(linkedlist *list) {
     node *curr = list->head;
     while (curr->next != list->head) {
@@ -86,5 +117,33 @@ int main() {
 
     printf("printbackward: ");
     printbackward(list);
+
+    list = prepend(list, 3);
+    list = prepend(list, 8);
+
+    printf("printforward: ");
+    printforward(list);
+
+    printf("printbackward: ");
+    printbackward(list);
+
+    printf("delete 10\n");
+    list = deleteforward(list, 10);
+
+    printf("printforward: ");
+    printforward(list);
+
+    printf("printbackward: ");
+    printbackward(list);
+
+    printf("delete 8\n");
+    list = deleteforward(list, 8);
+
+    printf("printforward: ");
+    printforward(list);
+
+    printf("printbackward: ");
+    printbackward(list);
+
     return 0;
 }
